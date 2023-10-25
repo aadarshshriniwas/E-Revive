57ca5b0b9dd656eba5d628576bfac2e219dac8aa
Aadarsh Shriniwas <aadarshshriniwas@gmail.com>
2023-10-25 09:47:47 +0530

    git token

--- lib/features/Map/map_screen.dart
+++ lib/features/Map/map_screen.dart
@@ -7,7 +7,6 @@ class MapPage extends StatefulWidget {
   @override
   _MapPageState createState() => _MapPageState();
 }
-
 class _MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
